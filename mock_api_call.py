51c80e9babc8c6a06f708957688393f820741b6e
“Mohit <mohitshekhawat84@gmail.com>
2026-01-12 19:39:48 -0600

    Implemented flask API to get official name of any country

--- mock_api_call.py
+++ mock_api_call.py
@@ -3,17 +3,23 @@
 import requests
 import os
 from dotenv import load_dotenv
+from flask import Flask, app
 
 # Load environment variables
 load_dotenv()
-url = os.getenv("URL_FOR_API")
+app = Flask(__name__)
+base_url = os.getenv("URL_FOR_API")
 # Make a GET request to the REST API
-print(f"Making request to URL: {url}")
-response = requests.get(url)
-output = response.json()
-official_name = output[0]['name']['official']
-# Print the response
-if response.status_code == 200:
-    print(f"Official Name: {official_name}")
-else:
-    print(f"Error: {response.status_code}") 
+@app.route('/country_info/<country_name>', methods=['GET'])
+def get_country_info(country_name):
+    url = base_url + country_name
+    print(f"Making request to base URL: {url}")
+    response = requests.get(url)
+    output = response.json()
+    official_name = output[0]['name']['official']
+    # Print the response
+    if response.status_code == 200:
+        print(f"Official Name: {official_name}")
+        return official_name
+    else:
+        print(f"Error: {response.status_code}") 
